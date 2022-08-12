d25ea3b486c8094385aca0e2bbcfe852ff23a580
Lorn Hin Adrian Lam <adrianlam@g.ucla.edu>
2022-08-12 23:14:23 +0100

    Main setup of Tkinter window
    A little wonky looking window setup (need to fix transparency)

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from tkinter import *
-print("hello world!")
+import tkinter as tk
+
+# Tkinter window screen setup
+root = tk.Tk()
+root.title("Tamagoch-deez")
+root.resizable(False, False) # so Window cannot be resized
+root.geometry(f'{root.winfo_screenwidth()-10}x{root.winfo_screenheight()-72}+0+0')
+root.wm_attributes('-transparentcolor','#add123')
+
+
+# Running App loop
+root.mainloop()
