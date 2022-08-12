5ad3c9b80ca1fd641855f3e2a0a8ccf20940d4f7
Lorn Hin Adrian Lam <adrianlam@g.ucla.edu>
2022-08-12 23:39:24 +0100

    Fixed transparency bug
    Transparency works!!! Test text is centered within tkinter window and will always be in front of all apps i.e. the overlay effect

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Tamagoch-deez")
 root.resizable(False, False) # so Window cannot be resized
-root.geometry(f'{root.winfo_screenwidth()-10}x{root.winfo_screenheight()-72}+0+0')
-root.wm_attributes('-transparentcolor','#add123')
+root.geometry(f'{root.winfo_screenwidth()-10}x{root.winfo_screenheight()-72}+0+0') # fit to window screen
+# window is in front of all apps always and fullscreen i.e. no window topbar
+root.attributes('-topmost',1)
+root.wm_attributes('-fullscreen', 'True')
 
+label= Label(root, text="This is a New Line Text", font= ('Helvetica 14 bold'), foreground= "red3")
+label.place(relx = 0.5, rely = 0.5, anchor ='center')
+
+# Make the window transparent
+root.config(bg = '#add123')
+root.wm_attributes('-transparentcolor','#add123')
 
 # Running App loop
 root.mainloop()
